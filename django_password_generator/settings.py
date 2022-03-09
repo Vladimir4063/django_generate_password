501064cd0097a93b90c7b51d87f381b31d3503d8
Vladimir4063 <vladimirgutierrez4063@gmail.com>
2022-03-09 12:58:20 -0300

    Se comento llave secreta

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xs&%qfzp%m+f4@251)o2jwq+6dd=s!tjjpesvxusy(hi8@qt0-'
+# SECRET_KEY = 'django-insecure-xs&%qfzp%m+f4@251)o2jwq+6dd=s!tjjpesvxusy(hi8@qt0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
